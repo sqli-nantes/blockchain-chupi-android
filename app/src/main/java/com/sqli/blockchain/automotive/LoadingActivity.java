f2d4e88e5616b6faed6c82feeb57c109ad1c493d
Nicolas Guillaume <gunicolas@sqli.com>
2016-08-02 10:17:17 +0200

    Utilisation de la librairie AltBeacon pour détecter le rpi et récupérer l'url + récupération des infos et de l'image en HTTP à l'adresse diffusée par le beacon

--- app/src/main/java/com/sqli/blockchain/automotive/LoadingActivity.java
+++ app/src/main/java/com/sqli/blockchain/automotive/LoadingActivity.java
@@ -1,25 +1,36 @@
 package com.sqli.blockchain.automotive;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.le.BluetoothLeScanner;
-import android.bluetooth.le.ScanCallback;
-import android.bluetooth.le.ScanResult;
-import android.bluetooth.le.ScanSettings;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 
-import com.github.ybq.android.spinkit.SpinKitView;
-import com.zl.reik.dilatingdotsprogressbar.DilatingDotsProgressBar;
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.utils.UrlBeaconUrlCompressor;
+
+import java.util.Collection;
 
 /**
  * Created by gunicolas on 28/07/16.
  */
-public class LoadingActivity extends AppCompatActivity {
+public class LoadingActivity extends AppCompatActivity implements BeaconConsumer, RangeNotifier {
 
-    BluetoothAdapter mBluetoothAdapter;
-    BluetoothLeScanner mBluetoothLeScanner;
-    ScanCallback mScanCallback;
+    public static final String INTENT_EXTRA_ADDRESS = "ADDRESS";
+
+    private static final double DISTANCE = 0.005;
+    private static final String DEVICE_NAME = "choupette";
+
+
+    BeaconManager beaconManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,46 +42,62 @@ public class LoadingActivity extends AppCompatActivity {
 
     private void startBluetoothScanner(){
 
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        beaconManager = BeaconManager.getInstanceForApplication(this.getApplicationContext());
+        beaconManager.setForegroundScanPeriod(100L); //1 period scan duration
+        beaconManager.setForegroundBetweenScanPeriod(1L); //period between 2 scan
+        beaconManager.getBeaconParsers().clear();
+        beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout("s:0-1=feaa,m:2-2=10,p:3-3:-41,i:4-20v"));
+        beaconManager.bind(this);
 
-        mBluetoothLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
+    }
 
-        mScanCallback = new ScanCallback() {
+    @Override
+    public void onBeaconServiceConnect() {
 
-            @Override
-            public void onScanResult(int callbackType, ScanResult result) {
-                super.onScanResult(callbackType, result);
+        Region region = new Region("all-beacons-region",null,null,null);
+        try{
+            beaconManager.startRangingBeaconsInRegion(region);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        beaconManager.addRangeNotifier(this);
+    }
 
-                int rssi = result.getRssi();
-                int txPower = result.getScanRecord().getTxPowerLevel();
 
-                double distance = Utils.beaconDistance(txPower,rssi);
-                if( distance < 0.8 ){
+    @Override
+    public void didRangeBeaconsInRegion(Collection<Beacon> collection, Region region) {
+        for(final Beacon b : collection){
 
-                    mBluetoothLeScanner.stopScan(mScanCallback);
+            if( b.getBluetoothName().equals(DEVICE_NAME) && b.getDistance() < DISTANCE ) {
 
-                    Intent intent = new Intent(LoadingActivity.this,DetectedCarActivity.class);
-                    startActivity(intent);
-                    overridePendingTransition(0,0);
-                }
+                beaconManager.unbind(this);
+
+                final String url = UrlBeaconUrlCompressor.uncompress(b.getId1().toByteArray());
+
+                Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                v.vibrate(200);
+
+                Intent intent = new Intent(LoadingActivity.this,DetectedCarActivity.class);
+                intent.putExtra(INTENT_EXTRA_ADDRESS,url);
+                startActivity(intent);
+                overridePendingTransition(0,0);
             }
-        };
-
-        ScanSettings.Builder mBuilder = new ScanSettings.Builder();
-        mBuilder.setReportDelay(0);
-        mBuilder.setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY);
-
-
-        mBluetoothLeScanner.startScan(mScanCallback);
+        }
     }
 
     @Override
     public void onBackPressed() {}
 
     @Override
-    protected void onDestroy() {
-        mBluetoothLeScanner.stopScan(mScanCallback);
+    protected void onPause() {
+        beaconManager.unbind(this);
+        super.onPause();
+    }
 
+    @Override
+    protected void onDestroy() {
+        beaconManager.unbind(this);
         super.onDestroy();
     }
 }
