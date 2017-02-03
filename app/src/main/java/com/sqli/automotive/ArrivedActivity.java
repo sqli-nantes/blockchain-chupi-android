a45ea96447bbccbbaf983e9d419e1c959277a25d
alebars <alebars@sqli.com>
2017-02-03 11:30:15 +0100

    remove unused xml files. update package path

--- app/src/main/java/com/sqli/automotive/ArrivedActivity.java
+++ app/src/main/java/com/sqli/automotive/ArrivedActivity.java
@@ -76,9 +76,9 @@ public class ArrivedActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        menu.removeItem(R.id.action_refresh);
-        menu.removeItem(R.id.action_back);
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        menu.removeItem(R.id.action_refresh);
+//        menu.removeItem(R.id.action_back);
         return true;
     }
 
